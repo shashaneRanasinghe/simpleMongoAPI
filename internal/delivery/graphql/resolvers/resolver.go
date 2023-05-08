ea67691a666379f348c7d13792ccc2308cc093f8
shashaneRanasinghe <shashane.ranasinghe@gmail.com>
2023-05-08 18:12:40 +0530

    updated to accomodate Lecturer

--- internal/delivery/graphql/resolvers/resolver.go
+++ internal/delivery/graphql/resolvers/resolver.go
@@ -2,6 +2,7 @@ package resolvers
 
 import (
 	"github.com/shashaneRanasinghe/simpleMongoAPI/internal/repository"
+	"github.com/shashaneRanasinghe/simpleMongoAPI/internal/usecases/lecturer"
 	"github.com/shashaneRanasinghe/simpleMongoAPI/internal/usecases/student"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -11,14 +12,21 @@ import (
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 type Resolver struct {
-	studentUsecase student.StudentUsecase
+	studentUsecase  student.StudentUsecase
+	lecturerUsecase lecturer.LecturerUsecase
 }
 
 func NewResolver(client *mongo.Client) *Resolver {
 	studentDBCollection := client.Database("SchoolDB").Collection("students")
 	studentRepo := repository.NewStudentRepo(client, studentDBCollection)
 	studentUsecase := student.NewStudent(studentRepo)
+
+	lecturerDBCollection := client.Database("SchoolDB").Collection("lecturer")
+	lecturerRepo := repository.NewLecturerRepo(client, lecturerDBCollection)
+	lecturerUsecase := lecturer.NewLecturer(lecturerRepo)
+
 	return &Resolver{
-		studentUsecase: studentUsecase,
+		studentUsecase:  studentUsecase,
+		lecturerUsecase: lecturerUsecase,
 	}
 }
